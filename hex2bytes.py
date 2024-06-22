844ae6d6b46c3439ee9dc46a52880972c5bbdb33
yntha <126660548+yntha@users.noreply.github.com>
2024-06-21 21:38:09 -0400

    Fix regex

--- hex2bytes.py
+++ hex2bytes.py
@@ -13,7 +13,7 @@ INDENT_SIZE = 4
 def main(args: Namespace):
     if args.row_width % args.group_size:
         raise Exception("Column size must be a multiple of row width.")
-    
+
     data = bytes.fromhex(args.hex_string)
     code = args.var_name + f"bytes.fromhex(  # size: {hex(len(data))}({len(data)}) bytes\n"
 
@@ -30,7 +30,7 @@ def main(args: Namespace):
 
     if m:  # add the remaining bytes as the final row
         chunks.append(data[d * args.row_width:])
-    
+
     for i in range(len(chunks)):
         row = (" " * INDENT_SIZE) + '"'
 
@@ -38,10 +38,10 @@ def main(args: Namespace):
             if j != 0 and j % args.group_size == 0:
                 row += " "
             row += "%02x" % b
-        
+
         if len(row) < last_line_len:
             row += " " * (last_line_len - len(row))
-        
+
 
         last_line_len = len(row)
         code += row + '"'
@@ -55,21 +55,21 @@ def main(args: Namespace):
                         code += chr(b)
                     else:
                         code += "."
-                
+
                 if args.offsets:
                     if len(chunks[i]) != args.row_width:
                         code += " " * (args.row_width - len(chunks[i]))
-                    
+
                     code += " | "
-            
+
             if args.offsets:
                 offset = i * args.row_width
                 code += f"0x{offset:0{data_nibbles}x}"
-        
+
         code += "\n"
-    
+
     code += ")"
-    
+
     indent = ((" " * INDENT_SIZE) * args.indent_level)
     code = ''.join([indent + line + "\n" for line in code.split("\n")])
 
@@ -87,16 +87,16 @@ def sanitize_hex_string(hexstr: str) -> str:
 
 
 def parse_file_info(file_info: str) -> tuple[str, int, int]:
-    file_path = file_info.split(":")
+    file_path = file_info.rsplit(":", 1)
 
     if len(file_path) == 1:
         return (file_path[0], 0, 0)
 
-    match = re.match(r"(?:([a-fA-F0-9x]+))?(?:\[([a-fA-F0-9x]+)\])?", file_path[1])
+    match = re.match(r"^(?:([a-fA-F0-9x]+))?(?:\[([a-fA-F0-9x]+)\])?$", file_path[1])
 
     if match is None:
         raise Exception("Failed to parse file info.")
-    
+
     return (file_path[0], *match.groups())
 
 
@@ -110,7 +110,7 @@ def get_hex_from_file(file_info: str) -> tuple[str, int]:
         offset = int(offset)
     else:
         offset = 0
-    
+
     if length is not None and length.startswith("0x"):
         length = int(length, base=16)
     elif length is not None:
@@ -120,15 +120,15 @@ def get_hex_from_file(file_info: str) -> tuple[str, int]:
 
     if not file_path.is_file():
         raise Exception("File does not exist or is not a file.")
-    
+
     file_data: bytes = file_path.read_bytes()
 
     if len(file_data) < offset:
         raise Exception("Offset goes past file size.")
-    
+
     if len(file_data) < (offset + length):
         raise Exception("Length goes past file size.")
-    
+
     stop = (offset + length) if length else None
     return (file_data[offset:stop].hex(), offset)
 
@@ -190,12 +190,12 @@ def parse_args() -> Namespace:
 
     args = parser.parse_args()
 
-    if not re.match(r"[a-fA-F0-9x ]+", args.hex_or_file):
+    if not re.match(r"^[a-fA-F0-9x ]+$", args.hex_or_file):
         file_data, offset = get_hex_from_file(args.hex_or_file)
 
         if offset != 0 and len(args.v) == 0:
             args.v = "_" + hex(offset)
-        
+
         args.hex_or_file = file_data
     else:
         args.hex_or_file = sanitize_hex_string(args.hex_or_file)
